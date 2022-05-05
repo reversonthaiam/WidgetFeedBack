1c4e5a95fda04304a0f06683b9d7583075e483a4
Reverson Thaiam <reverson.santos@frotabank.com.br>
2022-05-04 22:48:42 -0300

    widget pronto

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -4,10 +4,17 @@ module.exports = {
     extend: {
       colors: {
         brand: {
+          300: '#997dff',
           500: '#8257e6',
         }
+      },
+      borderRadios: {
+        md: '4px',
       }
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('tailwind-scrollbar'),
+  ],
 }
